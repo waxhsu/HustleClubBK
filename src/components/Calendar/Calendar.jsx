e81db87cf8b77368d5bd684095fa5f06e84091b6
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-11-06 09:08:17 -0500

    events page V1

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -9,10 +9,14 @@ export default function Calendar() {
                 {EVENT_info.map((data, index) => {
                     return (
                         <div className='eventCard'>
-                            <img src={data.src} />
+                            <img className="eventImg" src={data.src} />
                             <div className="eventDate">{data.date}</div>
-                            <div className="eventSpecial">{data.special}</div>
-
+                            <div className="eventName">{data.eventName}</div>
+                            {data.special && <div className="eventSpecial">SPECIAL EVENT</div>}
+                            <div className="eventDesc">{data.eventDesc}</div>
+                            <div className="classTime">Class {data.classTime}</div>
+                            <div className="sessionTime">Session {data.sessionTime}</div>
+                            <div className="location">{data.location}</div>
                         </div>
                     );
                 })}
